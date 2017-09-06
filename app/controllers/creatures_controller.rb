99b3a6620a167097f2c806901f46b1221f433d92
MikeNewm32 <michaelnewman85@hotmail.com>
2017-09-06 12:41:17 -0400

    create and show redirect working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,5 +1,5 @@
 class CreaturesController < ApplicationController
-    
+
     def index
         @creatures = Creature.all
     end
@@ -12,8 +12,13 @@ class CreaturesController < ApplicationController
         creature_params = params.require(:creature).permit(:name, :description)
         creature = Creature.new(creature_params)
         if creature.save
-            redirect_to creatures_path
+            redirect_to creature_path(creature)
         end
+    end
+
+    def show
+        creature_id = params[:id]
+        @creature = Creature.find_by_id(creature_id)
     end
 
 end
